89d3d68e6e38ba38275071ae1110e6e26e8e8832
Miguel Ángel Reyes Méndez <49988347+migreydev@users.noreply.github.com>
2025-01-08 11:33:35 +0100

    añadiendo filtro

--- src/components/population/population.jsx
+++ src/components/population/population.jsx
@@ -3,8 +3,10 @@ import "./population.css";
 
 function Population() {
   const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
+  const [populationFilter, setPopulationFilter] = useState("");
 
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
@@ -16,6 +18,7 @@ function Population() {
       })
       .then((data) => {
         setCountries(data);
+        setFilteredCountries(data);
         setLoading(false);
       })
       .catch((error) => {
@@ -23,6 +26,19 @@ function Population() {
         setLoading(false);
       });
   }, []);
+
+  const handlePopulationFilter = () => {
+    if (populationFilter === "" || isNaN(populationFilter)) {
+      setFilteredCountries(countries);
+    } else {
+      const filtered = countries.filter(
+        (country) =>
+          country.population ===
+          parseInt(populationFilter.replace(/\./g, ""), 10)
+      );
+      setFilteredCountries(filtered);
+    }
+  };
 
   if (loading) {
     return <p>Cargando...</p>;
@@ -35,22 +51,31 @@ function Population() {
   return (
     <div>
       <h1>Lista de Países</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Filtrar por población exacta"
+          value={populationFilter}
+          onChange={(e) => setPopulationFilter(e.target.value)}
+        />
+        <button onClick={handlePopulationFilter}>Filtrar</button>
+      </div>
       <table>
         <thead>
           <tr>
             <th>Nombre</th>
             <th>Capital</th>
-            <th>Region</th>
-            <th>Population</th>
+            <th>Región</th>
+            <th>Población</th>
           </tr>
         </thead>
         <tbody>
-          {countries.map((country) => (
+          {filteredCountries.map((country) => (
             <tr key={country.cca3}>
               <td>{country.name.common}</td>
-              <td>{country.capital}</td>
+              <td>{country.capital ? country.capital[0] : "N/A"}</td>
               <td>{country.region}</td>
-              <td>{country.population}</td>
+              <td>{country.population.toLocaleString()}</td>
             </tr>
           ))}
         </tbody>
